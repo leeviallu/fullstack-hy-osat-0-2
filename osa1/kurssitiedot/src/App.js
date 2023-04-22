a467adbab660f6614e8fc19d82b00157d1fec72e
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-22 22:34:57 +0300

    Componenttejen siirto

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,3 +1,34 @@
+const Header = ({course}) => {
+  const {name} = course;
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  const {name, exercises} = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Total = ({parts}) => {
+  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -16,38 +47,6 @@ const App = () => {
       }
     ]
   }
-
-  const Header = ({course}) => {
-    const {name} = course;
-    return (
-      <h1>{name}</h1>
-    )
-  }
-
-  const Content = ({parts}) => {
-
-    return (
-      <div>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} />
-      </div>
-    )
-  }
-
-  const Part = ({part}) => {
-    const {name, exercises} = part;
-    return (
-      <p>
-        {name} {exercises}
-      </p>
-    )
-  }
-
-  const Total = ({parts}) => {
-    return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  }
-
 
   const {parts} = course;
   return (
--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = ({course}) => {
+  const {name} = course;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{name}</h1>
+  )
 }
 
-export default App;
+const Content = ({parts}) => {
+
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  const {name, exercises} = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Total = ({parts}) => {
+  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const {parts} = course;
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
