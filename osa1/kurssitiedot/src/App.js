b6141180916d034cf2304120fd8da8dd9d34a74a
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-22 18:34:26 +0300

    Tehtävät 1.3-1.5

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,62 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const Header = ({course}) => {
+    const {name} = course;
+    return (
+      <h1>{name}</h1>
+    )
+  }
+
+  const Content = ({parts}) => {
+
+    return (
+      <div>
+        <Part part={parts[0]} />
+        <Part part={parts[1]} />
+        <Part part={parts[2]} />
+      </div>
+    )
+  }
+
+  const Part = ({part}) => {
+    const {name, exercises} = part;
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    )
+  }
+
+  const Total = ({parts}) => {
+    return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  }
+
+
+  const {parts} = course;
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
