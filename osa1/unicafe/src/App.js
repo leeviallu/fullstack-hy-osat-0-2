2274b334ed013f9ee89f08e97e3234654fc738f2
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-22 19:35:28 +0300

    HandleClick siistimmäksi

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -40,22 +40,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodClick = () => {
-    setGood(good + 1)
-  }
-  const neutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-  const badClick = () => {
-    setBad(bad + 1)
-  }
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={goodClick} text='good' />
-      <Button handleClick={neutralClick} text='neutral' />
-      <Button handleClick={badClick} text='bad' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
