4a34a8d2e121aef80f5a375c20a13fe4b4edb670
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-24 19:20:51 +0300

    Show button lisätty

--- osa2/maiden-tiedot/src/ListOfCountries.js
+++ osa2/maiden-tiedot/src/ListOfCountries.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import CountryPage from "./CountryPage";
+
 const ListOfCountries = ({list}) => {
+    const [showCountry, setShowCountry] = useState(false);
+    const [countryData, setCountryData] = useState([])
+
+    const handleClick = (country) => {
+        setShowCountry(true)
+        setCountryData(country)
+    }
     return (
         <div>   
             {
+                !showCountry 
+                ?
                 list.map(country => {
-                    return <p key={country.name.common}>{country.name.common}</p>
-                  })
-            }
+                    return (
+                        <div key={country.name.common}>
+                            <p>{country.name.common}</p>
+                            <button onClick={() => handleClick(country)}>show</button>
+                        </div>
+                )})
+                :
+                <CountryPage country={countryData}/>
+
+            }       
         </div>
     )
 }
