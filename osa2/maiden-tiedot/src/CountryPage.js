d5e04da97e54f643666fe7992552bb09272c98b9
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-24 20:47:10 +0300

    Osio 2 valmis

--- osa2/maiden-tiedot/src/CountryPage.js
+++ osa2/maiden-tiedot/src/CountryPage.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 const CountryPage = ({country}) => {
-    console.log(country)
+    const api_key = process.env.REACT_APP_API_KEY
+    const latitude = country.capitalInfo.latlng[0]
+    const longitude = country.capitalInfo.latlng[1]
+    const [temperature, setTemperature] = useState(null)
+    const [iconUrl, setIconUrl] = useState(null)
+    const [windSpeed, setWindSpeed] = useState(null)
+    const [altIcon, setAltIcon] = useState(null)
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
+    
+    useEffect(() => {
+        axios
+            .get(weatherUrl)
+            .then(response => {
+                setTemperature((response.data.main.temp - 272.15).toFixed(2) );
+                setWindSpeed(response.data.wind.speed);
+                setIconUrl(`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+                setAltIcon(response.data.weather[0].description);
+            })
+    }, [weatherUrl])
+
     return (
         <div key={country.name.common}>
             <h1>{country.name.common}</h1>
@@ -14,6 +36,10 @@ const CountryPage = ({country}) => {
                 }
             </ul>
             <img alt={country.flags.alt} src={country.flags.png} />
+            <h3>Weather in </h3>
+            <p>temperature {temperature} Celcius</p>
+            <img alt={altIcon} src={iconUrl} />
+            <p>wind {windSpeed} m/s</p>
         </div>
     )
 }
